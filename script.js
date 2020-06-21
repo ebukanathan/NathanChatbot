2db824e6592703eeb4ddaa32790487659f670647
ebukanathan <ebukanathan@gmail.com>
2020-06-21 18:21:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const form = document.querySelector('form');
 const input_box = document.querySelector('input');
 
 const brains = [
-  '/brain.rive'
+  'https://gist.githubusercontent.com/awesammcoder/91e0f6c527bfdc03b8815289ca4af150/raw/6410ce00b7e1ea0dbd28be03b6eaab64252a841d/brain.rive'
 ];
 
 bot.loadFile(brains).then(botReady).catch(botNotReady);
@@ -32,9 +32,9 @@ function selfReply(message){
 
 function botReady(){
   bot.sortReplies();
-  botReply('Hello');
+  botReply('Hello,welcome');
 }
 
 function botNotReady(err){
   console.log("An error has occurred.", err);
-}d
+}
